096b8523c7cf74907bfd127273c6e0aae19356e2
brobinson <brobinson@jhacorp.com>
2025-07-04 01:55:50 -0400

    image asset handling

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     distDir: "dist", // Set the output directory to 'dist'
+    images: {
+        unoptimized: true, // Disable image optimization for Netlify
+    },
 };
 
 export default nextConfig;
